1112336163a3d09f9f265a8c2866de8587f65827
Ryan Harter <ryanjharter@gmail.com>
2014-04-03 08:54:00 -0500

    Adds stubbed rename.py file for renaming tv show files.

---
+++ rename.py
@@ -0,0 +1,9 @@
+"""Script to rename a tv episode file based on information from theTVDB.org"""
+import sys
+
+def main():
+	"""Main entry point for the script."""
+	pass
+
+if __name__ == '__main__':
+	sys.exit(main())
